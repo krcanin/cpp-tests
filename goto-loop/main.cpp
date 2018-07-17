f40dba0d79b65f61048f7ec39eb83f732652448a
krcanin <marino.zirks@gmail.com>
2018-07-17 21:06:54 +0200

    a

--- goto-loop/main.cpp
+++ goto-loop/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, world!";
+    int begin = 00;
+    int end = 10;
+
+    START:
+        if(begin < end) {
+            std::cout << begin;
+            begin = begin + 1;
+            goto START;
+        }
 
     return 0;
 }
