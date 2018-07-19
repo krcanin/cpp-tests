67610bf810f07a9b2ff0b5ff73019d66907a3c27
krcanin <marino.zirks@gmail.com>
2018-07-19 13:07:53 +0200

    a

--- zondaken/libs/binary_tree/binary_heap/binary_heap.h
+++ zondaken/libs/binary_tree/binary_heap/binary_heap.h
@@ -12,12 +12,17 @@ namespace crap {
 
     template<class Key, class Value>
     class binary_heap_t {
-        private:
-            list_t<binary_heap_node<Key, Value>*> _data;
+        public:
+            list_t<binary_heap_node_t<Key, Value>*> _data;
             uint32_t _count = 0;
             std::function<int32_t(Key, Key)> _compare;
 
-            void build_max_heap();
+            uint32_t heap_size();
+            uint32_t left(uint32_t i);
+            uint32_t parent(uint32_t i);
+            uint32_t right(uint32_t i);
+
+            void build_heap();
             void heapify(uint32_t i);
         public:
             uint32_t& length = _count;
@@ -38,7 +43,7 @@ namespace crap {
             // USER-DEFINED
             // -----------
 
-            binary_heap_t(std::function<Key, Value> compare = [](Key x, Key y) { return x - y; });
+            binary_heap_t(std::function<int32_t(Key, Key)> compare = [](Key x, Key y) { return x - y; });
 
             std::pair<Key, Value> extract();
             void insert(Key key, Value value);
@@ -50,49 +55,47 @@ namespace crap {
     // -----------
 
     template<class Key, class Value>
+    uint32_t binary_heap_t<Key, Value>::heap_size() {
+        return _count;
+    }
+
+    template<class Key, class Value>
+    uint32_t binary_heap_t<Key, Value>::left(uint32_t i) {
+        return (2 * i) + 1;
+    }
+
+    template<class Key, class Value>
+    uint32_t binary_heap_t<Key, Value>::parent(uint32_t i) {
+        return (i - 1) / 2;
+    }
+
+    template<class Key, class Value>
+    uint32_t binary_heap_t<Key, Value>::right(uint32_t i) {
+        return (2 * i) + 2;
+    }
+
+    template<class Key, class Value>
     void binary_heap_t<Key, Value>::build_heap() {
-        uint32_t i = (_count / 2) - 1;
-
-        bool flag = true;
-
-        while (i >= 0 && flag) {
+        for(uint32_t i = (_count / 2) - 1; i >= 0; i -= 1) {
             heapify(i);
-
-            if (i == 0) {
-                flag = false;
-            } else {
-                i -= 1;
-            }
         }
     }
 
     template<class Key, class Value>
     void binary_heap_t<Key, Value>::heapify(uint32_t i) {
-    	uint32_t n = _length;
+        // https://www.youtube.com/watch?v=B7hVxCmfPtM
 
-    	uint32_t l = 2 * i + 1;
-    	uint32_t r = l + 1;
+        if(i < (heap_size() / 2)) {
+            auto& A = _data;
 
-    	if(i < n) {
-    		uint32_t max = 0;
-    		bool flag = false;
-
-    		if(l < n && r < n) {
-    			max = _compare(_data[l]->key, _data[r]->key) < 0 ? l : r;
-    			flag = true;
-    		} else if(l < n) {
-    			max = l;
-    			flag = true;
-    		} else if(r < n) {
-    			max = r;
-    			flag = true;
-    		}
-
-    		if(flag && _compare(_data[max]->key, _data[i]->key) < 0) {
-    			std::swap(_data[i], _data[max]);
-    			heapify(max);
-    		}
-    	}
+            if(A[i] < A[left(i)]) {
+                std::swap(A[i], A[left(i)]);
+                heapify(left(i));
+            } else if(A[i] < A[right(i)]) {
+                std::swap(A[i], A[right(i)]);
+                heapify(right(i));
+            }
+        }
     }
 
     // -----------
@@ -144,22 +147,28 @@ namespace crap {
     // -----------
 
     template<class Key, class Value>
-    binary_heap_t<Key, Value>::binary_heap_t(std::function<Key, Value> compare) : _compare(compare) {}
+    binary_heap_t<Key, Value>::binary_heap_t(std::function<int32_t(Key, Key)> compare) : _compare(compare) {}
 
     template<class Key, class Value>
     std::pair<Key, Value> binary_heap_t<Key, Value>::extract() {
         std::pair<Key, Value> result = peek();
         _data.pop(0);
         _count -= 1;
-        build_max_heap(0, _count);
+        build_heap();
     }
 
     template<class Key, class Value>
     void binary_heap_t<Key, Value>::insert(Key key, Value value) {
-        _data.append(new binary_heap_t<Key, Value>(key, value));
-        uint32_t i = _count;
-		build_max_heap(0, _count);
+        _data.append(new binary_heap_node_t<Key, Value>(key, value));
 		_count += 1;
+
+        auto& A = _data;
+        uint32_t i = parent(heap_size() - 1);
+
+        while(i >= 0 && A[i] > A[parent(i)]) {
+            std::swap(A[i], A[parent(i)]);
+            i = parent(i);
+        }
     }
 
     template<class Key, class Value>
