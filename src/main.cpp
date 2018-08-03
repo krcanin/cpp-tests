21a8329beca06192725e854862f7921ee27fdf20
zondaken <marino.zirks@gmail.com>
2018-08-04 00:07:32 +0200

    a

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,6 @@
 #include <iostream>
 
-#include "../core/java_string/java_string.h"
-#include "../core/smart_pointer/sp.h"
-
-class Starter {
-    public:
-        static void main(int argc, char** argv) {
-            
-        }
-};
-
 int main(int argc, char** argv) {
-    Starter::main(argc, argv);
+    
     return 0;
 }
